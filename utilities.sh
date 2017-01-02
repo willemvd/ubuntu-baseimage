429930079189453d82fea656b8e2f3ab56003692
willemvd <willem@opencirclesolutions.nl>
2017-01-02 14:26:59 +0100

    add rsync

--- utilities.sh
+++ utilities.sh
@@ -4,5 +4,5 @@ source /bd_build/buildconfig
 set -x
 
 ## Often used tools.
-$minimal_apt_get_install curl less vim-tiny psmisc
+$minimal_apt_get_install curl less vim-tiny psmisc rsync
 ln -s /usr/bin/vim.tiny /usr/bin/vim
